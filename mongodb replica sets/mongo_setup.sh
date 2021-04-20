87a023c347f9151e59e68003e93e683b25ff97a1
Tom Nagle <tomanagle@gmail.com>
2021-04-20 15:08:22 +1000

    feat: mongodb replicate sets

---
+++ mongodb replica sets/mongo_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "sleeping for 10 seconds"
+sleep 10
+
+echo mongo_setup.sh time now: `date +"%T" `
+mongo --host mongo1:27017 <<EOF
+  var cfg = {
+    "_id": "rs0",
+    "version": 1,
+    "members": [
+      {
+        "_id": 0,
+        "host": "mongo1:27017",
+        "priority": 2
+      },
+      {
+        "_id": 1,
+        "host": "mongo2:27017",
+        "priority": 0
+      },
+      {
+        "_id": 2,
+        "host": "mongo3:27017",
+        "priority": 0
+      }
+    ]
+  };
+  rs.initiate(cfg);
+EOF
